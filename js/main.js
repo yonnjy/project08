0f3236fe974633d276110f71c9c8ee66d71b66b4
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-06-27 09:41:11 +0900

    230627
    230627

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $('.main_slide').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
+});
