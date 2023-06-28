2ba91ed12dc73dfa25ac56c053557b4f284ae73a
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-06-28 18:20:50 +0900

    230628
    230628

--- js/main.js
+++ js/main.js
@@ -2,10 +2,24 @@ $(function () {
     $('.main_vi .main_slide').slick({
         arrows: false,
         dots: true,
-        fade: true,
+        // fade: true,
         autoplay: true,
         autoplaySpeed: 5000,
+        speed: 1000,
         pauseOnFocus: false,
         pauseOnHover: false,
+    });
+
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        const current = $('.main_slide .slick-current');
+
+        current.addClass('on').siblings().removeClass('on');
+    });
+
+    $('.main_vi .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev');
+    });
+    $('.main_vi .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext');
     });
 });
