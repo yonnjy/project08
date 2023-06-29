fe27f48ab21f131c3a4c91b67744fbc467ce1693
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-06-30 00:05:52 +0900

    230630
    230630

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function () {
+    $('.main_slide').on('init afterChange', function () {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    });
+    
     $('.main_vi .main_slide').slick({
         arrows: false,
         dots: true,
@@ -10,16 +15,22 @@ $(function () {
         pauseOnHover: false,
     });
 
-    $('.main_slide').on('init afterChange', function (e, s, c) {
-        const current = $('.main_slide .slick-current');
-
-        current.addClass('on').siblings().removeClass('on');
-    });
-
     $('.main_vi .arrows .left').on('click', function () {
         $('.main_slide').slick('slickPrev');
     });
     $('.main_vi .arrows .right').on('click', function () {
         $('.main_slide').slick('slickNext');
+    });
+
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.to_top').fadeIn(50);
+        } else {
+            $('.to_top').fadeOut(50);
+        }
     });
 });
